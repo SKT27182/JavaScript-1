323869ec119623f7507a9f339afe84fb2aaa62a5
Shailja Kant Tiwari <smartshailjakant1998@gmail.com>
2021-08-02 14:42:07 +0530

    created two function one with argument and another with return

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ var Byebye = function() {
 }
 
 Byebye();
+
+function sing(song){
+    console.log(song);
+}
+
+sing("Laa De Daaa");
+sing("haaaa maaaa");
+
+function multiply(a,b){
+    return a*b;
+}
+
+multiply(5,6);
