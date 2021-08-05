52effe005edc999bbbc84a13d8542d62fb2d11f9
Shailja Kant Tiwari <smartshailjakant1998@gmail.com>
2021-08-05 21:00:21 +0530

    Added a key listener and made too add a list by entering data in textField

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-var button = document.getElementsByTagName("button")[0];
+var button = document.getElementsByClassName("enter")[0];
+var input = document.getElementById("user-input");
+var ul = document.querySelector("ul");
 
-button.addEventListener("click", function(){
-	console.log("CLICK!!!");
-})
+function getLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick(){
+	if (getLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterEnter(event){
+	if(getLength() > 0 && event.key=== "Enter"){
+		createListElement();
+	}
+}
+
+
+button.addEventListener("click",addListAfterClick);
+
+input.addEventListener("keypress", addListAfterEnter);
