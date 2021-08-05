402de99601ca4f3e7099e03d5bb5bc143943ab7e
Shailja Kant Tiwari <smartshailjakant1998@gmail.com>
2021-08-05 17:23:41 +0530

    Added a mouse click Listener on a Button

--- script.js
+++ script.js
@@ -1,52 +1,5 @@
-var database = [
-	{
-		username: "skt",
-		password: "tiwari1"
-	},
-	{
-		username: "roli",
-		password: "tiwari2"
-	},
-	{
-		username: "priti",
-		password: "tiwari3"
-	}
-];
+var button = document.getElementsByTagName("button")[0];
 
-var newsfeed = [
-	{
-		username: "Bobby",
-		timeline: "So tired from all that learning!"
-	},
-	{
-		username: "Sally",
-		timeline: "Javascript is sooooo cool!"
-	},
-	{
-		username: "Mitch",
-		timeline: "Javascript is preeetyy cool!"
-	}
-];
-
-var userNamePrompt = prompt("What's your username ?");
-var passwordPrompt = prompt("What's your password?");
-
-function isUserValid(username, password){
-	for(var i=0; i< database.length; i++){
-		if(username === database[i].username && password === database[i].password){
-			return true;
-		}
-	}
-	return false;
-}
-
-function signIn(user, pass) {
-	if(isUserValid(user, pass)){
-		console.log(newsfeed);
-	}else{
-		alert("wrong");
-	}
-		
-}
-
-signIn(userNamePrompt, passwordPrompt);
+button.addEventListener("click", function(){
+	console.log("CLICK!!!");
+})
