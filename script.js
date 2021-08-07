82ebff488cc8c4956b4ed7d4578e06dc83b5ef72
Shailja Kant Tiwari <smartshailjakant1998@gmail.com>
2021-08-07 13:09:47 +0530

    Stuck in Delete Button

--- script.js
+++ script.js
@@ -2,30 +2,50 @@ var button = document.getElementsByClassName("enter")[0];
 var input = document.getElementById("user-input");
 var ul = document.querySelector("ul");
 
+
 function getLength() {
 	return input.value.length;
 }
 
+
+
+
 function createListElement() {
 	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
+	var delbtn = document.createElement("button");
+	ul.appendChild(li);
+	li.appendChild(document.createTextNode(input.value + " "));
+	li.appendChild(delbtn);
+	delbtn.appendChild(document.createTextNode("Delete"))
 	ul.appendChild(li);
 	input.value = "";
 }
 
-function addListAfterClick(){
+function addListAfterClick() {
 	if (getLength() > 0) {
 		createListElement();
 	}
 }
 
-function addListAfterEnter(event){
-	if(getLength() > 0 && event.key=== "Enter"){
+function addListAfterEnter(event) {
+	if (getLength() > 0 && event.key === "Enter") {
 		createListElement();
 	}
 }
 
 
-button.addEventListener("click",addListAfterClick);
+button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterEnter);
+
+
+var delbtns = document.getElementsByTagName("button");
+
+function removeParentElement(evt) {
+	console.log(evt.path[0]);
+	var bt =  evt.path[0];
+	
+
+for (var i = 0; i < delbtns.length; i++) {
+	delbtns[i].addEventListener("click", removeParentElement);
+}
